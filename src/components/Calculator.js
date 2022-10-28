8c2f2eb8ef01d6a3c5d3c2af78f835bbb05da7f6
hopeforkoami <hopeforkoami@gmail.com>
2022-10-28 20:43:52 +0000

    Refactor using hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,80 +1,74 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
 // eslint-disable-next-line
-class Calculator extends Component {
-  constructor() {
-    super();
-    this.state = {
-      total: 0,
-      next: '',
-      operation: '',
-    };
-  }
+function Calculator() {
+  const [state, updatestate] = useState({
+    total: 0,
+    next: '',
+    operation: '',
+  });
 
-  actionHandler = (e) => {
-    const calcul = calculate(this.state, e.target.value);
-    this.setState(calcul);
-  }
+  const actionHandler = (e) => {
+    const calcul = calculate(state, e.target.value);
+    updatestate(calcul);
+  };
 
-  renderTdElement = (classValue, valeur, action) => (
+  const renderTdElement = (classValue, valeur, action) => (
     <td className={classValue}>
       <button type="button" value={valeur} onClick={action}>{valeur}</button>
     </td>
-  )
+  );
 
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div>
-        <div className="calc-container">
-          <table className="calculator">
-            <tbody>
-              <tr>
-                <td colSpan={4} className="result">
-                  {total}
-                  {operation}
-                  {next}
-                </td>
-              </tr>
-              <tr>
-                {this.renderTdElement('num', 'AC', this.actionHandler)}
-                {this.renderTdElement('num', '+/-', this.actionHandler)}
-                {this.renderTdElement('num', '%', this.actionHandler)}
-                {this.renderTdElement('action', '÷', this.actionHandler)}
-              </tr>
-              <tr>
-                {this.renderTdElement('num', '7', this.actionHandler)}
-                {this.renderTdElement('num', '8', this.actionHandler)}
-                {this.renderTdElement('num', '9', this.actionHandler)}
-                {this.renderTdElement('action', 'x', this.actionHandler)}
-              </tr>
-              <tr>
-                {this.renderTdElement('num', '4', this.actionHandler)}
-                {this.renderTdElement('num', '5', this.actionHandler)}
-                {this.renderTdElement('num', '6', this.actionHandler)}
-                {this.renderTdElement('action', '-', this.actionHandler)}
-              </tr>
-              <tr>
-                {this.renderTdElement('num', '1', this.actionHandler)}
-                {this.renderTdElement('num', '2', this.actionHandler)}
-                {this.renderTdElement('num', '3', this.actionHandler)}
-                {this.renderTdElement('action', '+', this.actionHandler)}
-              </tr>
-              <tr>
-                <td className="num" colSpan={2}>
-                  <button type="button" value="0" onClick={this.actionHandler}>0</button>
+  return (
+    <div>
+      <div className="calc-container">
+        <table className="calculator">
+          <tbody>
+            <tr>
+              <td colSpan={4} className="result">
+                {state.total}
+                {state.operation}
+                {state.next}
+              </td>
+            </tr>
+            <tr>
+              {renderTdElement('num', 'AC', actionHandler)}
+              {renderTdElement('num', '+/-', actionHandler)}
+              {renderTdElement('num', '%', actionHandler)}
+              {renderTdElement('action', '÷', actionHandler)}
+            </tr>
+            <tr>
+              {renderTdElement('num', '7', actionHandler)}
+              {renderTdElement('num', '8', actionHandler)}
+              {renderTdElement('num', '9', actionHandler)}
+              {renderTdElement('action', 'x', actionHandler)}
+            </tr>
+            <tr>
+              {renderTdElement('num', '4', actionHandler)}
+              {renderTdElement('num', '5', actionHandler)}
+              {renderTdElement('num', '6', actionHandler)}
+              {renderTdElement('action', '-', actionHandler)}
+            </tr>
+            <tr>
+              {renderTdElement('num', '1', actionHandler)}
+              {renderTdElement('num', '2', actionHandler)}
+              {renderTdElement('num', '3', actionHandler)}
+              {renderTdElement('action', '+', actionHandler)}
+            </tr>
+            <tr>
+              <td className="num" colSpan={2}>
+                <button type="button" value="0" onClick={actionHandler}>0</button>
 
-                </td>
-                {this.renderTdElement('num', '.', this.actionHandler)}
-                {this.renderTdElement('action', '=', this.actionHandler)}
-              </tr>
-            </tbody>
-          </table>
-        </div>
+              </td>
+              {renderTdElement('num', '.', actionHandler)}
+              {renderTdElement('action', '=', actionHandler)}
+            </tr>
+          </tbody>
+        </table>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Calculator;
