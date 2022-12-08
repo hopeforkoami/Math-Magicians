d4a12c5a84bc6a1ab3f8256874340dfa6af31aaa
hopeforkoami <hopeforkoami@gmail.com>
2022-12-08 18:57:39 +0000

    Test user interaction

--- src/tests/Calcul.test.js
+++ src/tests/Calcul.test.js
@@ -1,8 +1,16 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Calcul from '../pages/Calcul';
 
 test('render Calcul component', () => {
   const { asFragment } = render(<Router><Calcul /></Router>);
   expect(asFragment()).toMatchSnapshot();
+});
+test('testing user interaction with buttons', () => {
+  const clickHandler = jest.fn();
+  render(<button type="button" value={5} onClick={clickHandler}>5</button>);
+  const btn = screen.getByText('5');
+  userEvent.click(btn);
+  expect(clickHandler).toHaveBeenCalled();
 });
